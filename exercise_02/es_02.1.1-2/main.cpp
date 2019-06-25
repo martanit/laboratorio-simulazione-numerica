8ca0b4e070db9efadea66d89998b4f63266517ef
martanit <marti.crip@gmail.com>
2019-06-25 18:07:52 +0200

    Complete es 2

--- exercise_02/es_02.1.1-2/main.cpp
+++ exercise_02/es_02.1.1-2/main.cpp
@@ -8,118 +8,137 @@ _/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
 *****************************************************************
 *****************************************************************/
 
-//ES 01.1.3
-//Martina Crippa
+// ES 02.1
+// Martina Crippa
 
-#include <iostream>
-#include <fstream>
-#include <string>
+#include "random/random.h"
 #include <cmath>
-#include <array>
-#include "random.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <vector>
 
-double dev_st_mean(unsigned int , double ,  double );
+// Functions
+double error(double sum, double sum2, int n);
+std::tuple<std::vector<double>, std::vector<double>>
+blocking_method(std::vector<double> v, unsigned int n_step,
+                unsigned int n_blocks);
+Random random_stuff();
 
-int main (int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 
-   // Blocks
-   const long unsigned int N(100);
-   // Throws
-   const long unsigned int M(10E7);
-   
-   double block_avg(0.);
-   double sum(0.), sum2(0.);
-   
-   std::array<double, N> I{};
-   std::array<double, N> err{};
+  const unsigned int n_blocks(100), n_throws(1E7);
 
-   double block_avg_nu(0.);
-   double sum_nu(0.), sum2_nu(0.);
-   
-   std::array<double, N> I_nu{};
-   std::array<double, N> err_nu{};
+  std::vector<double> I, err, I_nu, err_nu;
+  std::vector<double> r, r_nu;
 
-   // Random stuff
-   Random rnd;
-   int seed[4];
-   int p1, p2;
+  // Random stuff
+  Random rnd;
+  rnd = random_stuff();
+  double importance_sampled;
 
-   // To generate random number with random class 
-   std::ifstream Primes("Primes");
-   if ( Primes.is_open() ) Primes >> p1 >> p2 ;
-   else std::cerr << "PROBLEM: Unable to open Primes" << std::endl;
-   Primes.close();
+  for (unsigned int i = 0; i < n_throws; ++i) {
+    // Sampling distribution p(x)=2*(1-x)
+    importance_sampled = rnd.P();
 
-   std::ifstream input("seed.in");
-   std::string property;
-   if ( input.is_open() ){
-      while ( !input.eof() ){
-         input >> property;
-         if( property == "RANDOMSEED" ){
-            input >> seed[0] >> seed[1] >> seed[2] >> seed[3];
-            rnd.SetRandom(seed,p1,p2);
-         }
-      }
-      input.close();
-   } else std::cerr << "PROBLEM: Unable to open seed.in" << std::endl;
+    r.push_back(M_PI / 2. * std::cos(M_PI / 2. * rnd.Rannyu()));
+    r_nu.push_back(M_PI / 2. * std::cos(M_PI * importance_sampled / 2.) /
+                   (2 * (1 - importance_sampled)));
+  }
 
-   // Monte carlo integration for M steps
-   for (unsigned int j=0; j<N; ++j) {
-      block_avg = 0.;
-      block_avg_nu = 0.;
-      for (unsigned int i=0; i<M/N; ++i) {
-        block_avg += M_PI/2.*std::cos(M_PI/2.*rnd.Rannyu());
-        block_avg_nu += (1-std::pow(M_PI,2)/24.)*M_PI/2.*std::cos(M_PI/2.*rnd.P());
-      }
+  // Perform block statistics
+  I = std::get<0>(blocking_method(r, n_throws, n_blocks));
+  err = std::get<1>(blocking_method(r, n_throws, n_blocks));
 
-      // Evaluate integral and statistical uncertain every M/N steps
-      // for N blocks
-      block_avg /= double(M/N);
-      sum += block_avg;
-      sum2 += std::pow(block_avg,2);
-     
-      I.at(j)=sum/double(j+1);
-      err.at(j)=dev_st_mean(j+1, sum, sum2);
-      
-      block_avg_nu /= double(M/N);
-       
-      std::cout << block_avg_nu << std::endl;
+  I_nu = std::get<0>(blocking_method(r_nu, n_throws, n_blocks));
+  err_nu = std::get<1>(blocking_method(r_nu, n_throws, n_blocks));
 
-      sum_nu += block_avg_nu;
-      sum2_nu += std::pow(block_avg_nu,2);
-     
-      I_nu.at(j)=sum_nu/double(j+1);
-      err_nu.at(j)=dev_st_mean(j+1, sum_nu, sum2_nu);
-   }
-  
-   // Print interval, progressive chi mean "output_es02.1.1.dat"
-   std::ofstream output("../data/output_es02.1.1.dat");
-   if ( output.is_open() ){
-    for(unsigned int i=0; i<N; ++i)
-        output << i+1 << " " <<  I[i] << " " << err[i] << std::endl;
-   } 
-   
-   else std::cerr << "PROBLEM: Unable to open output file" << std::endl;
-   
-   output.close();   
-   
-   // Print interval, progressive chi mean "output_es02.1.2.dat"
-   std::ofstream output2("../data/output_es02.1.2.dat");
-   if ( output2.is_open() ){
-      for(unsigned int i=0; i<N; i++)
-        std::cout << i+1 << " " <<  I_nu[i] << " " << err_nu[i] << std::endl;
-   } 
-   
-   else std::cerr << "PROBLEM: Unable to open output file" << std::endl;
-   
-   output2.close();   
-   
-   return 0;
+  // Print interval, progressive chi mean "output_es02.1.1.dat"
+  std::ofstream output("../data/output_es02.1.1.dat");
+  if (output.is_open()) {
+    for (unsigned int i = 0; i < n_blocks; ++i)
+      output << i + 1 << " " << I[i] << " " << err[i] << std::endl;
+  }
+  else
+    std::cerr << "PROBLEM: Unable to open output file" << std::endl;
+  output.close();
+
+  // Print interval, progressive chi mean "output_es02.1.2.dat"
+  std::ofstream output2("../data/output_es02.1.2.dat");
+  if (output2.is_open()) {
+    for (unsigned int i = 0; i < n_blocks; i++)
+      output2 << i + 1 << " " << I_nu[i] << " " << err_nu[i] << std::endl;
+  }
+  else
+    std::cerr << "PROBLEM: Unable to open output file" << std::endl;
+  output2.close();
+
+  return 0;
 }
 
-double dev_st_mean(unsigned int n, double sum, double sum2){
-  if(n==1) return 0.;
-  else return std::sqrt((sum2/double(n) - std::pow(sum/double(n), 2))/double(n-1));
+double error(double sum, double sum2, int n) {
+  if (n == 0)
+    return 0;
+  else
+    return sqrt((sum2 - pow(sum, 2)) / n);
+}
+
+std::tuple<std::vector<double>, std::vector<double>>
+blocking_method(std::vector<double> v, unsigned int n_step,
+                unsigned int n_blocks) {
+
+  std::vector<double> mean, mean2, err_prog;
+  double sum(0), sum_prog(0), sum2_prog(0);
+
+  unsigned int blk_step = int(n_step / n_blocks);
+
+  // Loop over block
+  for (unsigned int i = 0; i < n_blocks; i++) {
+    // Reset partial sum
+    sum = 0.;
+    // Loop over step in each block
+    for (unsigned int j = 0; j < blk_step; ++j)
+      sum += v[j + i * blk_step];
+    // Progressive mean and error of block means
+    sum_prog += sum / double(blk_step);
+    sum2_prog += std::pow(sum / double(blk_step), 2);
+
+    mean.push_back(sum_prog / double(i + 1));
+    mean2.push_back(sum2_prog / double(i + 1));
+    err_prog.push_back(error(mean[i], mean2[i], i));
+  }
+  return std::make_tuple(mean, err_prog);
+}
+
+Random random_stuff() {
+  // Random stuff
+  Random rnd;
+  int seed[4];
+  int p1, p2;
+
+  // to generate random number with random class
+  std::ifstream Primes("random/Primes");
+  if (Primes.is_open())
+    Primes >> p1 >> p2;
+  else
+    std::cerr << "PROBLEM: Unable to open Primes" << std::endl;
+  Primes.close();
+
+  std::ifstream input("random/seed.in");
+  std::string property;
+  if (input.is_open()) {
+    while (!input.eof()) {
+      input >> property;
+      if (property == "RANDOMSEED") {
+        input >> seed[0] >> seed[1] >> seed[2] >> seed[3];
+        rnd.SetRandom(seed, p1, p2);
+      }
+    }
+    input.close();
+  } else
+    std::cerr << "PROBLEM: Unable to open seed.in" << std::endl;
+  return rnd;
 }
 /****************************************************************
 *****************************************************************
--- exercise_02/es_02.2/main.cpp
+++ exercise_02/es_02.2/main.cpp
@@ -8,136 +8,152 @@ _/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
 *****************************************************************
 *****************************************************************/
 
-//ES 01.1.3
-//Martina Crippa
+// ES 02.2
+// Martina Crippa
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <cmath>
+#include "random/random.h"
 #include <array>
-#include "random.h"
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <string>
 
 struct point;
 struct angle;
-double dev_st_mean(unsigned int ,double ,double );
-point discrete_walk(double ,double ,point );
-point continus_walk(angle , double , point );
+double error(double sum, double sum_sq, int n);
+Random random_stuff();
+point discrete_walk(double, double, point);
+point continus_walk(angle, double, point);
 
-int main (int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 
-   // Monte Carlo steps
-   const unsigned int N(10000);
-   // Number of steps
-   const unsigned int Nstep(100);
-   // Lattice constant
-   double a(1.);
-   
-   point p_d, p_c;
-   std::array<double, Nstep> sum_d{0};
-   std::array<double, Nstep> sum2_d{0};
-   std::array<double, Nstep> sum_c{0};
-   std::array<double, Nstep> sum2_c{0};
-   
-   double r2_d(0), r2_c(0);
+  // Monte Carlo steps
+  const unsigned int N(1E4), n_step(1E2);
+  // Lattice constant
+  double a(1.);
 
-   // random stuff
-   Random rnd;
-   int seed[4];
-   int p1, p2;
+  point p_d, p_c;
+  std::array<double, n_step> sum_d{0};
+  std::array<double, n_step> sum2_d{0};
+  std::array<double, n_step> sum_c{0};
+  std::array<double, n_step> sum2_c{0};
 
-   // to generate random number with random class 
-   std::ifstream Primes("Primes");
-   if ( Primes.is_open() ) Primes >> p1 >> p2 ;
-   else std::cerr << "PROBLEM: Unable to open Primes" << std::endl;
-   Primes.close();
+  double r2_d(0), r2_c(0);
 
-   std::ifstream input("seed.in");
-   std::string property;
-   if ( input.is_open() ){
-      while ( !input.eof() ){
-         input >> property;
-         if( property == "RANDOMSEED" ){
-            input >> seed[0] >> seed[1] >> seed[2] >> seed[3];
-            rnd.SetRandom(seed,p1,p2);
-         }
-      }
-      input.close();
-   } else std::cerr << "PROBLEM: Unable to open seed.in" << std::endl;
-  
-   for (unsigned int j=0; j<N; ++j) {
-      // Repeat RW from the origin
-      p_d={0., 0., 0.};
-      p_c={0., 0., 0.};
-      for (unsigned int i=0; i<Nstep; i++) {
-        p_d = discrete_walk(rnd.Dice(), a, p_d);
-        r2_d = p_d.x*p_d.x+p_d.y*p_d.y+p_d.z*p_d.z;
-        sum_d[i] += r2_d;
-        sum2_d[i] += std::pow(r2_d, 2);
+  // random stuff
+  Random rnd;
+  rnd = random_stuff();
 
-        p_c = continus_walk(rnd.Sphere(), a, p_c);
-        r2_c = p_c.x*p_c.x+p_c.y*p_c.y+p_c.z*p_c.z;
-        sum_c[i] += r2_c;
-        sum2_c[i] += std::pow(r2_c, 2);
-      }
-   }
-   
-   // Print interval, progressive chi mean "output_es02.2.1.dat"
-   std::ofstream output("../data/output_es02.2.1.dat");
-   if ( output.is_open() ){
-      for(unsigned int i=0; i<Nstep; ++i)
-        output << i+1 << " " <<  std::sqrt(sum_d[i]/double(N)) 
-          << " " << dev_st_mean(i+1, sum_d[i]/double(N), sum2_d[i]/double(N)) << std::endl;
-   } 
-   
-   else std::cerr << "PROBLEM: Unable to open output file" << std::endl;
-   
-   output.close();   
-   
-   // Print interval, progressive chi mean "output_es02.2.2.dat"
-   std::ofstream output2("../data/output_es02.2.2.dat");
-   if ( output2.is_open() ){
-      for(unsigned int i=0; i<Nstep; ++i)
-        output2 << i+1 << " " <<  std::sqrt(sum_c[i]/double(N)) 
-          << " " << dev_st_mean(i+1, sum_c[i]/double(N), sum2_c[i]/double(N)) << std::endl;
-   } 
-   
-   else std::cerr << "PROBLEM: Unable to open output file" << std::endl;
-   
-   output2.close();   
+  for (unsigned int j = 0; j < N; ++j) {
+    // Repeat RW from the origin
+    p_d = {0., 0., 0.};
+    p_c = {0., 0., 0.};
+    for (unsigned int i = 0; i < n_step; i++) {
+      p_d = discrete_walk(rnd.Dice(), a, p_d);
+      r2_d = p_d.x * p_d.x + p_d.y * p_d.y + p_d.z * p_d.z;
+      sum_d[i] += r2_d;
+      sum2_d[i] += std::pow(r2_d, 2);
+
+      p_c = continus_walk(rnd.Sphere(), a, p_c);
+      r2_c = p_c.x * p_c.x + p_c.y * p_c.y + p_c.z * p_c.z;
+      sum_c[i] += r2_c;
+      sum2_c[i] += std::pow(r2_c, 2);
+    }
+  }
+
+  // Print interval, progressive chi mean "output_es02.2.1.dat"
+  std::ofstream output("../data/output_es02.2.1.dat");
+  if (output.is_open()) {
+    for (unsigned int i = 0; i < n_step; ++i)
+      output << i + 1 << " " << std::sqrt(sum_d[i] / double(N)) << " "
+             << error(std::sqrt(sum_d[i]) / double(N), std::sqrt(sum2_d[i]) / double(N), i + 1)
+             << std::endl;
+  } else
+    std::cerr << "PROBLEM: Unable to open output file" << std::endl;
+  output.close();
+
+  // Print interval, progressive chi mean "output_es02.2.2.dat"
+  std::ofstream output2("../data/output_es02.2.2.dat");
+  if (output2.is_open()) {
+    for (unsigned int i = 0; i < n_step; ++i)
+      output2 << i + 1 << " " << std::sqrt(sum_c[i] / double(N)) << " "
+              << error(std::sqrt(sum_c[i]) / double(N), std::sqrt(sum2_c[i]) / (double(N)), i + 1)
+              << std::endl;
+  } else
+    std::cerr << "PROBLEM: Unable to open output file" << std::endl;
+  output2.close();
+
   return 0;
 }
 
-double dev_st_mean(unsigned int n, double sum, double sum2){
-  if(n==1) return 0.;
-  else return std::sqrt((sum2/double(n) - std::pow(sum/double(n), 2))/double(n-1));
-}
-
-point discrete_walk(double dice, double step, point p){
+point discrete_walk(double dice, double step, point p) {
   double S(0.);
-  if( dice==1 or dice==2 ){
-    if( dice==1 ) S=1;
-    else S=0;
-  p.x+=2*step*(S-0.5);
+  if (dice == 1 or dice == 2) {
+    if (dice == 1)
+      S = 1;
+    else
+      S = 0;
+    p.x += 2 * step * (S - 0.5);
   }
-  if( dice==3 or dice==4){
-    if(dice==3) S=1;
-    else S=0;
-  p.y+=2*step*(S-0.5);
+  if (dice == 3 or dice == 4) {
+    if (dice == 3)
+      S = 1;
+    else
+      S = 0;
+    p.y += 2 * step * (S - 0.5);
   }
-  if(dice==5 or dice==6){
-    if(dice==5) S=1;
-    else S=0;
-  p.z+=2*step*(S-0.5); 
+  if (dice == 5 or dice == 6) {
+    if (dice == 5)
+      S = 1;
+    else
+      S = 0;
+    p.z += 2 * step * (S - 0.5);
   }
   return p;
 }
 
-point continus_walk(angle w, double step, point p){
-    p.x = p.x + step*std::sin(w.theta)*std::cos(w.phi);  
-    p.y = p.y + step*std::sin(w.theta)*std::sin(w.phi);
-    p.z = p.z + step*std::cos(w.theta);
-    return p;
+point continus_walk(angle w, double step, point p) {
+  p.x = p.x + step * std::sin(w.theta) * std::cos(w.phi);
+  p.y = p.y + step * std::sin(w.theta) * std::sin(w.phi);
+  p.z = p.z + step * std::cos(w.theta);
+  return p;
+}
+
+double error(double sum, double sum_sq, int n) {
+  if (n == 1)
+    return 0;
+  else
+    return sqrt((sum_sq / double(n) - pow(sum / double(n), 2)) / double(n - 1));
+}
+
+Random random_stuff() {
+  // Random stuff
+  Random rnd;
+  int seed[4];
+  int p1, p2;
+
+  // to generate random number with random class
+  std::ifstream Primes("random/Primes");
+  if (Primes.is_open())
+    Primes >> p1 >> p2;
+  else
+    std::cerr << "PROBLEM: Unable to open Primes" << std::endl;
+  Primes.close();
+
+  std::ifstream input("random/seed.in");
+  std::string property;
+  if (input.is_open()) {
+    while (!input.eof()) {
+      input >> property;
+      if (property == "RANDOMSEED") {
+        input >> seed[0] >> seed[1] >> seed[2] >> seed[3];
+        rnd.SetRandom(seed, p1, p2);
+      }
+    }
+    input.close();
+  } else
+    std::cerr << "PROBLEM: Unable to open seed.in" << std::endl;
+  return rnd;
 }
 /****************************************************************
 *****************************************************************
